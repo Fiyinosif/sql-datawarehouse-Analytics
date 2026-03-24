9549beb4c2994db0a6ca4769c2e8f322b389af4f
Fiyin Osifala <123388270+Fiyinosif@users.noreply.github.com>
2026-03-24 13:25:58 -0500

    Add ranking analysis SQL script for products and customers
    This script performs ranking analysis to identify top and bottom performers in terms of revenue and orders, utilizing both simple ranking and window functions.

---
+++ scripts/Analytics/ranking_analysis.sql
@@ -0,0 +1,90 @@
+/*
+===============================================================================
+Ranking Analysis
+===============================================================================
+Purpose:
+    - To identify top and bottom performers in the business.
+    - To rank products and customers based on revenue and orders.
+    - To practice ranking techniques using ORDER BY, TOP, and window functions.
+
+Tables Used:
+    - gold.fact_sales
+    - gold.dim_products
+    - gold.dim_customers
+
+Description:
+    This script performs ranking analysis to find the best and worst performing
+    products and customers. It uses simple ranking with TOP as well as advanced
+    ranking using window functions such as RANK(). This type of analysis is
+    commonly used in business reporting to identify high-value customers,
+    best-selling products, and low-performing items.
+
+===============================================================================
+*/
+
+
+
+-- Which 5 products Generating the Highest Revenue?
+-- Simple Ranking
+SELECT TOP 5
+    p.product_name,
+    SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+    ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC;
+
+-- Complex but Flexibly Ranking Using Window Functions
+SELECT *
+FROM (
+    SELECT
+        p.product_name,
+        SUM(f.sales_amount) AS total_revenue,
+        RANK() OVER (ORDER BY SUM(f.sales_amount) DESC) AS rank_products
+    FROM gold.fact_sales f
+    LEFT JOIN gold.dim_products p
+        ON p.product_key = f.product_key
+    GROUP BY p.product_name
+) AS ranked_products
+WHERE rank_products <= 5;
+
+-- What are the 5 worst-performing products in terms of sales?
+SELECT TOP 5
+    p.product_name,
+    SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+    ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue;
+
+-- Find the top 10 customers who have generated the highest revenue
+SELECT TOP 10
+    c.customer_key,
+    c.first_name,
+    c.last_name,
+    SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+    ON c.customer_key = f.customer_key
+GROUP BY 
+    c.customer_key,
+    c.first_name,
+    c.last_name
+ORDER BY total_revenue DESC;
+
+-- The 3 customers with the fewest orders placed
+SELECT TOP 3
+    c.customer_key,
+    c.first_name,
+    c.last_name,
+    COUNT(DISTINCT order_number) AS total_orders
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+    ON c.customer_key = f.customer_key
+GROUP BY 
+    c.customer_key,
+    c.first_name,
+    c.last_name
+ORDER BY total_orders ;
